081420fe9db4624c7ebcd62be1a7ce169c673468
Kevin <kgarcia0170@gmail.com>
2023-12-01 22:56:02 -0500

    hwday3

---
+++ hwday3pt2.py
@@ -0,0 +1,18 @@
+def sqrFootage(user_length, user_width):
+    print (user_length * user_width)
+    return user_length * user_width
+
+
+#sqrFootage(5, 7) 
+
+def circumference_circle(r):
+    PI = 3.14
+    C = 2 * PI * r
+    print(C)
+    return C
+     
+#circumference_circle(77)
+
+
+
+              
